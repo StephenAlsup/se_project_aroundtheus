beaebe7e62a81a0793d1a6dc7a33aaa2464be451
Stephen Alsup <stephenalsup87@gmail.com>
2023-07-05 01:57:54 -0700

    Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
   ];
+
+  const profileEditBtn = document.querySelector('#profile-edit-button');
+  const profileEditModal= document.querySelector('#profile-edit-modal');
+  const profileCloseBtn= document.querySelector('#profile-modal-close');
+  
+  
+  
+  profileEditBtn.addEventListener('click', () => {
+    profileEditModal.classList.add(modal__opened);
+  })
+
+  profileCloseBtn.addEventListener('click', closePopup);
